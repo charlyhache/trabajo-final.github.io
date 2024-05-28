7d2454a149ace069cd7f4b457e9a28d4ad5df28c
charlyhache <167575346+charlyhache@users.noreply.github.com>
2024-05-27 22:06:01 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+
+var edad=Number(prompt("por favor ingese edad"));
+if(edad>=18){
+    alert("puedes ingresar a nuestra pagina web");
+} else{
+    alert("Usted es menor de edad entra bajo su propia responsabilidad");
+}
